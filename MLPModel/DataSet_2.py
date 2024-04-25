36298d741e45208ef4a762bc6841adbbc4682ca0
Igor Staręga <2002gori@gmail.com>
2024-04-25 18:54:34 +0200

    added tensorflow models (kaggle data and eddited data)

--- MLPModel/DataSet_2.py
+++ MLPModel/DataSet_2.py
@@ -1,19 +1,3 @@
-import torch
-import pandas as pd
-import numpy as np
-from torch.utils.data import Dataset
-
-
-class TorchFacialFeaturesDataset(Dataset):
-    def __init__(self):
-        Xy = pd.read_csv("../Data/EditedData.csv.gz", compression="gzip")
-        self.n_samples = Xy.shape[0]
-        X = Xy.iloc[:, -1].apply(lambda x: np.fromstring(x, sep=" ", dtype=np.float64)).values/255
-        self.x = torch.tensor(np.stack(X))
-        self.y = torch.tensor(Xy.iloc[:, :-1].values)
-
-    def __len__(self):
-        return self.n_samples
-
-    def __getitem__(self, item):
-        return self.x[item], self.y[item]
+version https://git-lfs.github.com/spec/v1
+oid sha256:63881bf1e9cba963b0b388bbf31ab4bbc537313cb33bb648020fb98d9be23101
+size 594
--- MLPModel/Model_2.py
+++ MLPModel/Model_2.py
@@ -1,21 +1,3 @@
-import torch
-import torch.nn as nn
-
-
-class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(MLP, self).__init__()
-        self.l1 = nn.Linear(input_dim,1036)
-        self.relu1 = nn.ReLU()
-        self.l2 = nn.Linear(1036,1036)
-        self.relu2 = nn.ReLU()
-        self.l3 = nn.Linear(1036,output_dim)
-
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.relu1(out)
-        out = self.l2(out)
-        out = self.relu2(out)
-        out = self.l3(out)
-        return out
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:38f262796b9c0cb40f49a7ba60d12f2e713cad2b206aef56fcdb75dcf0a2d54a
+size 518
